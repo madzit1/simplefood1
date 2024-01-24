7866ecdfb9bf97877db760ce1016883768c8bb13
madzit1 <madzitsadoev1@gmail.com>
2024-01-24 21:02:14 +0200

    added content, restaurants-card, customer-slider, mobile-app, footer

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,13 @@
-$(function(){
+$(function () {
+
+    $('.customer-slider__inner').slick({
+        dots: true,
+        fade: true,
+        autoplay: true,
+        autoplaySpead: 2000,
+        prevArrow: '<button type="button"class="slick-prev slick-prev--active"><svg width="10" height="18"><use href="images/stack/sprite.svg#arrow-left"></use></svg></button>',
+        nextArrow: '<button type="button"class="slick-next"><svg  width="10" height="18"><use href="images/stack/sprite.svg#arrow-right"></use></svg><button>',
+    })
+
     var mixer = mixitup('.categories__content-list');
 });
