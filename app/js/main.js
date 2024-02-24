d9ecb0a7880baaa236753cc3068f1600f91aebc6
madzit1 <madzitsadoev1@gmail.com>
2024-02-25 00:21:12 +0200

    added header, hero, categories, advantages, product-card, restaurants, restaurants-card review, footer global

--- app/js/main.js
+++ app/js/main.js
@@ -7,20 +7,20 @@ $(function () {
         autoplaySpead: 2000,
         adaptiveHeight: true,
         autoHeight: true,
-            
+
         prevArrow: '<button class="arrow-button arrow-button--prew" type="button"><span class="sr-only">предыдущий слайд</span><svg class="arrow-button__icon" width="10" height="18"><use href="images/icons/sprite.svg#icon-arrow-left"></use></svg></button>',
         nextArrow: '<button class="arrow-button arrow-button--next" type="button"><span class="sr-only">следующий слайд</span><svg class="arrow-button__icon" width="10" height="18"><use href="images/icons/sprite.svg#icon-arrow-left"></use></svg><button>',
     });
 
     $(document).ready(function () {
         $('.header__btn').click(function (evenet) {
-            $('.header__btn,.mobile-menu,body').addClass('active')
+            $('.header__btn,.mobile-menu,.mobile-menu__top').addClass('active')
         });
     });
 
     $(document).ready(function () {
         $('.mobile-menu__btn').click(function (evenet) {
-            $('.header__btn,.mobile-menu,body').removeClass('active')
+            $('.header__btn,.mobile-menu,.mobile-menu__top').removeClass('active')
         });
     });
 
